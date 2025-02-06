d512c1bddc96e0e5a3ed641dfe3570f0985144db
hinzik <humphreydkwok@gmail.com>
2025-02-06 23:01:12 +0800

    updated theme, logo, favicon, and general settings

---
+++ README.md
@@ -0,0 +1,25 @@
+# The P.O.W.A. Project
+
+An open-source modern fullstack development course.
+
+## Contributing
+
+1. Fork the repository.
+2. Create a new branch (`git checkout -b feature-branch`).
+3. Make your changes.
+4. Commit your changes (`git commit -m 'Add some feature'`).
+5. Push to the branch (`git push origin feature-branch`).
+6. Open a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](./LICENSE) file for details.
+
+## Contact
+
+For support or inquiries, contact Humphrey Kwok [@hinzik] (contact@hinzik.dev) via email.
+
+## About the website template itself
+
+Generated from [rubix-documents](https://github.com/rubixvi/rubix-documents)
+Rubix Studios - [https://www.rubixstudios.com.au](https://www.rubixstudios.com.au)
---
+++ settings/settings.ts
@@ -0,0 +1,29 @@
+export const url = "https://powa.courses"
+export const siteicon = "/logo.svg"
+
+export const gtm = "GTM-XXXXXXX"
+export const gtmconnected = false
+
+export const sitename = "The POWA Project"
+export const description =
+  "The POWA Project is an open-source modern fullstack development course, focusing on theory and practise"
+export const keywords = [
+  "fullstack development course",
+  "documentation",
+  "The POWA Project",
+  "open source",
+]
+export const urlimage = ""
+export const imagealt = "The POWA Project"
+export const twitterhandle = "@humphreydkwok"
+
+export const companyname = "The POWA Project"
+export const companylink = "https://hinzik.dev"
+
+export const branding = true
+export const rightsidebar = true //on this page
+export const feedbackedit = true
+export const tableofcontent = true
+export const totopscroll = true
+
+export const loadfromgithub = false
